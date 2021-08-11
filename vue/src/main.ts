7136608e9509f8fa8fafa92fb36dbda2c0018eb1
dengyuxuan <dengyuxuan@kuaishou.com>
2021-08-11 12:04:39 +0800

    feat: init

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -7,7 +7,8 @@ import VueAxios from 'vue-axios'
 import './base.css'
 import 'element-plus/lib/theme-chalk/index.css';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+axios.defaults.baseURL = 'http://118.31.187.88'
 
 const App = createApp(app)
 App.use(router)
