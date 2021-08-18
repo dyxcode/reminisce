36eb1c8e76b953832b597d89e277f8ef922e1179
dengyuxuan <dengyuxuan@kuaishou.com>
2021-08-18 20:26:32 +0800

    feat: init

--- django/serenade/urls.py
+++ django/serenade/urls.py
@@ -5,6 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    re_path('^admin/', admin.site.urls),
+    re_path('^api/admin/', admin.site.urls),
     re_path('^api/', include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
