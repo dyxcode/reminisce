2029acca5a352d71dfdf8215c477cfd8e635328e
dengyuxuan <dengyuxuan@kuaishou.com>
2021-08-19 19:07:00 +0800

    feat: init

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,26 +1,27 @@
+import { defineAsyncComponent } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 export default createRouter({
   history: createWebHistory(),
   routes: [{
-      path: '/',
-      name: 'home',
-      component: () => import('../view/home/home.vue'),
+    path: '/',
+    name: 'home',
+    component: defineAsyncComponent(() => import('../view/home/home.vue')),
   }, {
     path: '/image',
     name: 'imagelist',
-    component: () => import('../view/image/imagelist.vue'),
+    component: defineAsyncComponent(() => import('../view/image/imagelist.vue')),
   }, {
     path: '/blog',
     name: 'bloglist',
-    component: () => import('../view/blog/bloglist.vue'),
+    component: defineAsyncComponent(() => import('../view/blog/bloglist.vue')),
   }, {
     path: '/blog/:id',
     name: 'blogdetail',
-    component: () => import('../view/blog/blogdetail.vue'),
+    component: defineAsyncComponent(() => import('../view/blog/blogdetail.vue')),
   }, {
     path: '/file',
     name: 'filelist',
-    component: () => import('../view/file/filelist.vue'),
+    component: defineAsyncComponent(() => import('../view/file/filelist.vue')),
   }]
 })
