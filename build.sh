4812514311d36f88bf5e5c1c3b43b6302ae9c3e5
dengyuxuan <dengyuxuan@kuaishou.com>
2021-08-20 19:16:56 +0800

    feat: init

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-rm -rf dist && cd vue && npm install && npm run build && mv dist/ ../dist/ && cd .. &&
-docker-compose up --build
+if $1 == 'vue' then
+  cd vue && npm run build && rm -rf  ../dist/* && mv dist/* ../dist/ && rmdir dist && cd ..
+else
+  cd vue && npm install && npm run build && mv dist/ ../dist/ && cd .. && docker-compose up --build
+fi
